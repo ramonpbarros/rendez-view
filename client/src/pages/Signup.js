582bddf7c8b2f09fd29be7f24bad0d1766b49e3e
Sorrab <ramonpbarros@gmail.com>
2020-05-14 22:45:52 -0700

    changes made to singup ui

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { Link, Redirect, useHistory } from "react-router-dom";
 import API from "./../utils/API";
-import {useAuth} from "../utils/auth";
-
+import { useAuth } from "../utils/auth";
 
 function Signup() {
   const [formState, setFormState] = useState({
@@ -19,18 +18,18 @@ function Signup() {
     return <Redirect to="/" />;
   }
 
-  const handleFormSubmit = event => {
+  const handleFormSubmit = (event) => {
     event.preventDefault();
     API.signUpUser(formState.username, formState.email, formState.password)
-      .then(res => {
+      .then((res) => {
         // once the user has signed up
         // send them to the login page
         history.replace("/login");
       })
-      .catch(err => alert(err));
+      .catch((err) => alert(err));
   };
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     const { name, value } = event.target;
     setFormState({
       ...formState,
@@ -39,49 +38,64 @@ function Signup() {
   };
 
   return (
-    <div className="container">
-      <h1>Signup</h1>
-      <form onSubmit={handleFormSubmit}>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            className="form-control"
-            placeholder="Username goes here..."
-            name="username"
-            type="text"
-            id="username"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email address:</label>
-          <input
-            className="form-control"
-            placeholder="Email goes here..."
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            className="form-control"
-            placeholder="Password goes here..."
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-      <p>
-        <Link to="/login">Go to Login</Link>
-      </p>
+    <div className="card">
+      <div className=" card-body container">
+        <h1
+          style={{ textAlign: "center", color: "#f54c4c", fontWeight: "bold" }}
+          className="mb-4"
+        >
+          Welcome
+        </h1>
+        <p style={{ textAlign: "center", color: "gray" }} className="mb-4">
+          In order to sign up, please fill out all the fields below.
+        </p>
+        <form onSubmit={handleFormSubmit}>
+          <div className="form-group">
+            {/* <label htmlFor="username">Username:</label> */}
+            <input
+              className="form-control"
+              placeholder="Username"
+              name="username"
+              type="text"
+              id="username"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            {/* <label htmlFor="email">Email address:</label> */}
+            <input
+              className="form-control"
+              placeholder="E-mail"
+              name="email"
+              type="email"
+              id="email"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            {/* <label htmlFor="pwd">Password:</label> */}
+            <input
+              className="form-control"
+              placeholder="Password"
+              name="password"
+              type="password"
+              id="pwd"
+              onChange={handleChange}
+            />
+          </div>
+          <div class="col text-center">
+            <button type="submit" className="btn btn-custom mb-3 mt-3">
+              Sing Up
+            </button>
+          </div>
+        </form>
+        <p style={{ textAlign: "center" }}>
+          <span style={{ color: "gray" }}>Already have an account? </span>
+          <Link to="/login" style={{ color: "grey", fontWeight: "bold" }}>
+            Login
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
