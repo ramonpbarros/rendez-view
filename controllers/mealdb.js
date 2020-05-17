cef75a31c4e0caa3ea09495fabe837114002b499
Sorrab <ramonpbarros@gmail.com>
2020-05-17 15:03:54 -0700

    created controllers for cocktail and meal set up connection w serverjs for cocktail

---
+++ controllers/mealdb.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/randommeal", (req, res) => {
+  axios
+    .get("https://www.themealdb.com/api/json/v1/1/random.php")
+    .then((response) => {
+      return res.json(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
