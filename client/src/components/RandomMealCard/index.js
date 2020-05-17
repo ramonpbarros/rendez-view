a701293e0a03fd0fe25c812c4481319395672cb1
Sorrab <ramonpbarros@gmail.com>
2020-05-17 00:38:58 -0700

    minor changes to meal card

--- client/src/components/RandomMealCard/index.js
+++ client/src/components/RandomMealCard/index.js
@@ -28,6 +28,39 @@ function RandomMealCard(props) {
           <p className="card-text">
             {props.measure5} {props.ingredient5}
           </p>
+          <p className="card-text">
+            {props.measure6} {props.ingredient6}
+          </p>
+          <p className="card-text">
+            {props.measure7} {props.ingredient7}
+          </p>
+          <p className="card-text">
+            {props.measure8} {props.ingredient8}
+          </p>
+          <p className="card-text">
+            {props.measure9} {props.ingredient9}
+          </p>
+          <p className="card-text">
+            {props.measure10} {props.ingredient10}
+          </p>
+          <p className="card-text">
+            {props.measure11} {props.ingredient11}
+          </p>
+          <p className="card-text">
+            {props.measure12} {props.ingredient12}
+          </p>
+          <p className="card-text">
+            {props.measure13} {props.ingredient13}
+          </p>
+          <p className="card-text">
+            {props.measure14} {props.ingredient14}
+          </p>
+          <p className="card-text">
+            {props.measure15} {props.ingredient15}
+          </p>
+          <p className="card-text">
+            {props.measure16} {props.ingredient16}
+          </p>
           <hr />
           <p>
             <strong>Instruction:</strong>
