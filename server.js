4d95f8baffabc61b171b1d3823782001247e8043
Nicolas Ayala <ayalanick84@gmail.com>
2020-05-17 17:08:33 -0700

    Deleted some useless code

--- server.js
+++ server.js
@@ -17,14 +17,7 @@ const PORT = process.env.PORT || 3001;
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 // Setting up express to use json and set it to req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
