823d6b5a6f1519b913c9097ffa8507b23ae18b0c
Sorrab <ramonpbarros@gmail.com>
2020-05-22 12:12:28 -0700

    footer created and added

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+import { FaGithubSquare } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer className="footer d-flex justify-content-center mt-5">
+      <a
+        href="https://github.com/ramonpbarros/rendez-view"
+        target="_blank"
+        className="fa zoomIcon mr-2 mt-2"
+        style={{color: "white"}}
+      >
+        <FaGithubSquare />
+      </a>
+      <p className="mt-2">© Copyright</p>
+    </footer>
+  );
+}
+
+export default Footer;
