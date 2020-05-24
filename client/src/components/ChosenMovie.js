93da0149f43d6f68a4a26254d566b4f69de02f61
Nicolas Ayala <ayalanick84@gmail.com>
2020-05-24 14:21:57 -0700

    Updated movie details style

--- client/src/components/ChosenMovie.js
+++ client/src/components/ChosenMovie.js
@@ -1,15 +1,70 @@
 import React from "react";
+import { FaArrowCircleLeft } from "react-icons/fa";
+
 function ChosenMovie(props) {
+  function goBack() {
+    window.history.back();
+  }
+
   return (
     <div className="container">
-      <div className="col-sm-6">
-        <img src={props.image} alt={props.title} />
+      <div
+        className="jumbotron container"
+        style={{
+          backgroundColor: "white",
+          paddingBottom: 0,
+          marginBottom: 0,
+        }}
+      >
+        <div className="mr-1 zoom1 mt-2" style={{ float: "left" }}>
+          <button style={{ color: "#f54c4c" }} onClick={goBack} className="btn">
+            <FaArrowCircleLeft size={40} />
+          </button>
+        </div>
+        <h1
+          className="display-4"
+          style={{ color: "#f54c4c", fontWeight: "bold" }}
+        >
+          {props.title}
+        </h1>
+        <hr className="my-4" />
       </div>
-      <div className="col-sm-6">
-        <div className="row">
-          <div className="col-sm-12">
-            <h1>{props.title}</h1>
-            <p>{props.plot}</p>
+      <div className="card mb-3">
+        <div className="row no-gutters">
+          <div className="col-sm-4">
+            <img
+              src={props.image}
+              alt="mealImg"
+              style={{
+                border: "solid 1",
+                borderRadius: 5,
+                maxHeight: "500px",
+                maxWidth: "300px",
+              }}
+              width="100%"
+            />
+          </div>
+          <div className="col-sm-4 text-center">
+            <h5 className="card-title">
+              <strong>Plot:</strong>
+            </h5>
+            <div className="col">
+              <p>{props.plot}</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="card-body">
+              <p className="card-text">
+                <strong>Released: </strong> {props.date}
+              </p>
+              <p className="card-text">
+                <strong>Runtime: </strong> {props.time} minutes
+              </p>
+
+              <a href={props.link} target="_blank">
+                Go to IMDB page
+              </a>
+            </div>
           </div>
         </div>
       </div>
--- client/src/pages/MoviePage.js
+++ client/src/pages/MoviePage.js
@@ -9,16 +9,31 @@ function MoviePage() {
   const [title, setTitle] = useState();
   const [image, setImage] = useState();
   const [plot, setPlot] = useState();
+  const [time, setTime] = useState();
+  const [date, setDate] = useState();
+  const [link, setLink] = useState();
 
   useEffect(() => {
     API.getOneMovie(id).then((res) => {
       setTitle(res.data.original_title);
-      setImage(` http://image.tmdb.org/t/p/w185/${res.data.backdrop_path}`);
+      setImage(` http://image.tmdb.org/t/p/w185/${res.data.poster_path}`);
       setPlot(res.data.overview);
+      setTime(res.data.runtime);
+      setDate(res.data.release_date);
+      setLink(`https://www.imdb.com/title/${res.data.imdb_id}/`);
     });
   }, []);
   function renderMovie() {
-    return <ChosenMovie title={title} plot={plot} image={image} />;
+    return (
+      <ChosenMovie
+        title={title}
+        plot={plot}
+        image={image}
+        time={time}
+        date={date}
+        link={link}
+      />
+    );
   }
   return <div>{renderMovie()}</div>;
 }
